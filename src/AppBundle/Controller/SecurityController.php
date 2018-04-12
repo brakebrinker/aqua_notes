ff6f854cde15939684bf513f47ba519f6f92570d
zumtum <drummen11@gmail.com>
2018-04-12 18:29:20 +0300

    added login page and form

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: m.pevnev
+ * Date: 12.4.18
+ * Time: 18.01
+ */
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Form\LoginForm;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function loginAction(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        $form = $this->createForm(LoginForm::class, [
+           '_username' => $lastUsername
+        ]);
+
+        return $this->render('security/login.html.twig', array(
+            'form' => $form->createView(),
+            'error'         => $error,
+        ));
+    }
+}
