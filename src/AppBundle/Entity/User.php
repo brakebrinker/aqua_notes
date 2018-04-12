fb436cb153a4c4484d7c3cad8c16ea635ced2730
zumtum <drummen11@gmail.com>
2018-04-12 18:00:58 +0300

    create User class, db migrations and fixtures

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: m.pevnev
+ * Date: 12.4.18
+ * Time: 17.38
+ */
+
+namespace AppBundle\Entity;
+
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $email;
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getPassword()
+    {
+    }
+
+    public function getSalt()
+    {
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+}
